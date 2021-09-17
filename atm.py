880a37dcf70148927cf8e2a3720b839c3595b1fe
tarunkathuria1 <72028064+tarunkathuria1@users.noreply.github.com>
2021-09-17 11:26:48 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,22 @@
+class Atm(object):
+ """
+    blueprint for atm
+  """
+
+def __init__(self, useratm, cartnumber, cardpin,withdraw):
+    self.useratm = useratm
+    self.cartnumber = cartnumber
+    self.cardpin = cardpin
+    self . withdraw = withdraw
+    
+def cartnumber(self):
+    print("numbered")
+
+def cardpin(self):
+    print("pined")
+
+atm = card("12345", "0000", "10", 90)
+
+print(atm.number())
+
+
